b8d2266ee6152a94b56d6fd89b8cbf650fa6c5fe
andre-pm <andre.pereira.martins100@gmail.com>
2022-08-26 13:45:18 -0300

    Criação de Api

---
+++ Warren/Data/Repository/BaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseModel
+    {
+        public string Create(T entity)
+        {
+            return "Criado com sucesso";
+        }
+
+        public string Delete(int id)
+        {
+            return "Deletado com sucesso";
+        }
+
+        public List<T> GetAll()
+        {
+            List<T> list = new List<T>();
+            return list;
+        }
+
+        public T GetById(int id)
+        {
+            T entity = null;
+            return entity;
+        }
+
+        public string Update(T entity)
+        {
+            return "Atualizado com sucesso";
+        }
+    }
+}
---
+++ Warren/WebAPI/Controllers/AnimalController.cs
@@ -0,0 +1,27 @@
+﻿using Data.Model;
+using Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        [HttpGet]
+        public Animal Get()
+        {
+            Animal animal = new Animal { Id = 1, Especie = "Cachorro Caramelo", Nome = "Alfredo", Peso = 4.5f };
+            return animal;
+
+        }
+
+        [HttpPost]
+        public string Post(Animal model)
+        {
+            AnimalRepository repo = new AnimalRepository();
+            return repo.Create(model);
+        }
+    }
+}
+
